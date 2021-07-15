ce72074e575da91af177caa266fe3ed3cadf4273
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-07-15 11:05:08 +0200

    Remove old files

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $BASH_SOURCE)"
-chmod +x build.sh
-if [ -d compiler ]; then
-	rm -rf compiler
-fi
-git clone https://github.com/EarthOS-SMC/PowerSlash-userspace compiler
